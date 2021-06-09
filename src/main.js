35d217bd663acacf2c5ed3c00e5d0d7a97a6044a
Andre Reitz <andrereitz@live.com>
2021-06-09 15:44:20 -0300

    7 - Babel Polyfills, Transforms & Presets (BUG - Library updates necessary to corrent babel libraries)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+require('@babel/runtime/regenerator');
 require('./main.css');
 require('./images/test.jpg')
 // require('./index.html');
 
-var a = () => {
-    console.log('Hello from the future');
+var a = async (args) => {
+    const {a, b, c} = args;
+    await console.log('Hello from the future');
+    console.log('this will be converted', a, b, c)
 }
 
-a();
+a({a: 1, b: 2, c:'three'});
